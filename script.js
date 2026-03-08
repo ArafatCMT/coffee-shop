5adacfb2ca11552dc8b4fb518925d99e20ec658a
Arafat <arafatemon6969@gmail.com>
2026-03-08 12:00:37 +0600

    add testimonial section & link swiper slider

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ menuOpenButton.addEventListener("click", ()=>{
 
 // menuCloseButton e click korle menuOpenButton function call hobe 
 menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
+
+
+// Initialize swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween:25,
+  grabCursor:true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive Breakpoints
+  breakpoints:{
+  0:{
+    slidesPerView: 1
+  },
+  768:{
+    slidesPerView: 2
+  },
+  1024:{
+    slidesPerView: 3
+  },
+  }
+});
